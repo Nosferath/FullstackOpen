575c0bc9fe8eec47992348f621707ab287d4e1a3
Claudio Yanez <x_metalnos_x@hotmail.com>
2022-02-28 12:53:15 -0300

    Storing partial progress

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,43 +1,99 @@
-const Header = (props) => (
-    <h1>{props.course}</h1>
-);
+// const Header = (props) => {
+//   console.log('Header props:', props)
+//   return(
+//     <h1>{props.course.name}</h1>
+//   )
+// };
 
-const Content = (props) => {
+// const Part = (props) => {
+//   return(
+//     <p>{props.part.name} {props.part.exercises}</p>
+//   )
+// };
+
+// const Content = (props) => {
+//   console.log('Content props:', props)
+//   return (
+//     <div>
+//       <Part part={props.course.parts[0]} />
+//       <Part part={props.course.parts[1]} />
+//       <Part part={props.course.parts[2]} />
+//     </div>
+//   )
+// };
+
+// const Total = (props) => {
+//   console.log('Total props:', props)
+//   return (
+//     <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
+//   )
+// };
+
+// const App = () => {
+//   const course = {
+//     name: 'Half Stack application development',
+//     parts: [
+//       {
+//         name: 'Fundamentals of React',
+//         exercises: 10
+//       },
+//       {
+//         name:'Using props to pass data',
+//         exercises: 7
+//       },
+//       {
+//         name: 'State of a component',
+//         exercises: 14
+//       }
+//     ]
+//   };
+
+//   return (
+//     <div>
+//       <Header course={course} />
+//       <Content course={course} />
+//       <Total course={course} />
+//     </div>
+//   )
+// };
+
+// export default App;
+
+import { useState } from 'react'
+
+
+const Display = ({ counter }) => <div>{counter}</div>;
+
+
+const Button = ({ onClick, text }) => {
   return (
-    <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
-    </>
+    <button onClick={onClick}>
+      {text}
+    </button>
   )
-};
-
-const Total = (props) => (
-  <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-);
+}
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
+  
+  const handleLeftClick = () => {
+    setClicks({ ...clicks, left: clicks.left + 1 });
+  }
+
+  const handleRightClick = () => {
+    setClicks({ ...clicks, right: clicks.right + 1 });
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
     </div>
   )
 };
 
-export default App;
+export default App
